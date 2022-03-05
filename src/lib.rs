56a72462fbdc7db0c187ee3c9a77e7c511d6c0fe
Andreu Santaren Llop <andreu.santaren@gmail.com>
2022-03-05 18:03:38 +0100

    Improve docs.

--- src/lib.rs
+++ src/lib.rs
@@ -67,21 +67,49 @@ where
     Self: PartialEq + PartialOrd + Sized,
 {
     /// Value lies between 2 values, both included. Assumes left is smaller than right.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(10.between(0,10));
+    /// assert!(10.between(0,9) == false);
+    /// ```
     fn between(&self, left: Self, right: Self) -> bool {
         *self >= left && *self <= right
     }
 
     /// Value lies within 2 values, both excluded. Assumes left is smaller than right.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(5.within(0,10));
+    /// assert!(5.within(0,5) == false);
+    /// ```
     fn within(&self, left: Self, right: Self) -> bool {
         *self > left && *self < right
     }
 
     /// Value lies between 2 values, left included, right excluded. Assumes left is smaller than right.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(0.leftween(0,10));
+    /// assert!(10.leftween(0,10) == false);
+    /// ```
     fn leftween(&self, left: Self, right: Self) -> bool {
         *self >= left && *self < right
     }
 
     /// Value lies between 2 values, right included, left excluded. Assumes left is smaller than right.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(10.rightween(0,10));
+    /// assert!(0.rightween(0,10) == false);
+    /// ```
     fn rightween(&self, left: Self, right: Self) -> bool {
         *self > left && *self <= right
     }
@@ -97,24 +125,52 @@ where
     }
 
     /// Value lies between 2 values, both included. If left is bigger than right, swap order.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(10.ord_between(10,0));
+    /// assert!(10.ord_between(9,0) == false);
+    /// ```
     fn ord_between(&self, left: Self, right: Self) -> bool {
         let (left, right) = left.reorder(&right);
         self >= left && self <= right
     }
 
     /// Value lies within 2 values, both excluded. If left is bigger than right, swap order.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(5.ord_within(10,0));
+    /// assert!(5.ord_within(5,0) == false);
+    /// ```
     fn ord_within(&self, left: Self, right: Self) -> bool {
         let (left, right) = left.reorder(&right);
         self > left && self < right
     }
 
     /// Value lies between 2 values, left included, right excluded. If left is bigger than right, swap order.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(0.ord_leftween(10,0));
+    /// assert!(10.ord_leftween(10,0) == false);
+    /// ```
     fn ord_leftween(&self, left: Self, right: Self) -> bool {
         let (left, right) = left.reorder(&right);
         self >= left && self < right
     }
 
     /// Value lies between 2 values, right included, left excluded. If left is bigger than right, swap order.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(10.ord_rightween(10,0));
+    /// assert!(0.ord_rightween(10,0) == false);
+    /// ```
     fn ord_rightween(&self, left: Self, right: Self) -> bool {
         let (left, right) = left.reorder(&right);
         self > left && self <= right
@@ -145,6 +201,13 @@ where
     Self: PartialEq + PartialOrd + Sized,
 {
     /// Array contains the value.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(10.is_in(&[0,10,100]));
+    /// assert!(10.is_in(&[0,100]) == false);
+    /// ```
     fn is_in(&self, set: &[Self]) -> bool {
         if let None = set.iter().find(|&x| x == self) {
             false
@@ -155,6 +218,13 @@ where
     }
 
     /// Value lies between one of the ranges.
+    /// 
+    /// Example:
+    /// ```
+    /// # use cond_utils::*;
+    /// assert!(10.in_ranges(&[0..5,10..100]));
+    /// assert!(10.in_ranges(&[100..1000]) == false);
+    /// ```
     fn in_ranges(&self, ranges: &[Range<Self>]) -> bool {
         if let None = ranges.iter().find(|&x| x.contains(self)) {
             false
